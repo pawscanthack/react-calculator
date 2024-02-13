bb557a793daeb93f17492c5458402367ad545258
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2024-02-13 18:44:05 -0500

    Removed hyperlink

--- src/Video.js
+++ src/Video.js
@@ -6,7 +6,6 @@ export default function Video() {
       <video autoPlay loop muted id="video">
         <source src={backgroundVideo}></source>
       </video>
-      B-Roll provided by <a href="http://www.videezy.com">Videezy!</a>
     </>
   );
 }
